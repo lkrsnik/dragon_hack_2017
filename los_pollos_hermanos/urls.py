079dc272ca5d1907a293f4e5fb708a81354e6ae7
lkrsnik <krsnik.luka92@gmail.com>
2017-05-21 01:18:01 +0200

    Added background image

--- los_pollos_hermanos/urls.py
+++ los_pollos_hermanos/urls.py
@@ -4,6 +4,7 @@ from los_pollos_hermanos.views import AttackView, StatisticsView, \
     VisualisationView, AttackAPIView
 
 urlpatterns = [
+    url(r'^$', StatisticsView.as_view()),
     url(r'^statistics$', StatisticsView.as_view()),
     url(r'^attack$', AttackView.as_view()),
     url(r'^visualisation', VisualisationView.as_view()),
